c84044d419042fd8e3e1124c6f31b0365b746218
Saiteja <52398704+saiteja1698@users.noreply.github.com>
2019-08-07 23:58:11 +0530

    Create s3-p4.py

---
+++ s3-p4.py
@@ -0,0 +1,12 @@
+import sys,string,math
+n=int(input())
+m=2**n
+T=[]
+for i in range (0,m):
+  l=bin(i)[2:]
+  j=len(l)
+  if j < n:
+    l='0' *(n-j)+l
+    T.append(l)
+for i in range (0,len(T)):
+  print(T[i])   
